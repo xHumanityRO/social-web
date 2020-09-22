ddb0faa728020ad0d2a4ceecf7c031327b05240a
Loredana <eliaearth@yahoo.com>
2020-09-22 20:48:20 +0300

    get data

--- src/components/startPage.js
+++ src/components/startPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import getFeeds from '../utils/api';
+import { cl } from '../utils/utils';
 
 const StartPage = (props) => {
     const path = window.location.pathname;
     const userId = path.split('/').pop();
+cl(`----------------userID = ${userId} ---------------`)
+    const feeds = getFeeds(userId);
+    console.log('-----------------feeds--------')
+    console.dir(feeds);
     return (<div>
         ------Start page------
         userId : {userId}
---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export const cl = (param) => {
+    console.log(param);
+}
+
+export const cd = (param) => {
+    console.dir(param);
+}
+
