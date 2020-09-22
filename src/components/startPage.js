741ef26dc7e89ed46a70c4b10ae21cd70549b408
Loredana <eliaearth@yahoo.com>
2020-09-22 21:04:57 +0300

    shoow data

--- src/components/startPage.js
+++ src/components/startPage.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import getFeeds from '../utils/api';
+// import getFeeds from '../utils/api';
 import { cl } from '../utils/utils';
+import feedsxH00033 from '../mocks/feedsxH00033';
 
 const StartPage = (props) => {
     const path = window.location.pathname;
     const userId = path.split('/').pop();
 cl(`----------------userID = ${userId} ---------------`)
-    const feeds = getFeeds(userId);
+    const feeds = feedsxH00033;//getFeeds(userId);
     console.log('-----------------feeds--------')
     console.dir(feeds);
     return (<div>
         ------Start page------
         userId : {userId}
+
+        {feeds.firstName} {feeds.lastName}
+        {feeds.posts.map((feedItem) => {
+            return (<div>
+                {feedItem.id}<br/>
+                <img src={feedItem.picture} alt="post"/><br/>
+            <b>{feedItem.message}</b>
+                </div>)
+        })}
     </div>)
 
 }
---
+++ src/mocks/feedsxH00033.js
@@ -0,0 +1,133 @@
+const feedsxH00033 = {
+    "firstName": "Iulian",
+    "lastName": "Lungu",
+    "posts": [
+        {
+            "id": "3418160981537358_3002804269739700",
+            "picture": "https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-0/s130x130/91398052_1264924560564210_2568426336049692672_o.png?_nc_cat=103&_nc_sid=8024bb&_nc_ohc=CFMm93SFo_YAX8I8W1E&_nc_ht=scontent.fotp3-3.fna&oh=66fe9072c1a9a034033a187e8e733871&oe=5F8ED83C",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_2989783271041800",
+            "picture": null,
+            "message": null
+        },
+        {
+            "id": "3418160981537358_2492674080752724",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQCRh5y5tZ8bSpU_&w=130&h=130&url=https%3A%2F%2Fwww.votstrainatate.ro%2FImages%2Fvotaep_v2.jpg&cfs=1&_nc_cb=1&_nc_hash=AQBEWDMtUMLjUY9r",
+            "message": "https://www.votstrainatate.ro/"
+        },
+        {
+            "id": "3418160981537358_2389353731084760",
+            "picture": null,
+            "message": null
+        },
+        {
+            "id": "3418160981537358_2254591484560986",
+            "picture": "https://scontent.fotp3-2.fna.fbcdn.net/v/t1.0-0/s130x130/53803544_2254591454560989_3179097937384833024_o.jpg?_nc_cat=100&_nc_sid=110474&_nc_ohc=Z9fz3utVR2AAX9tVdTt&_nc_ht=scontent.fotp3-2.fna&tp=7&oh=9f4ffb045ccd4ebead8835889970f030&oe=5F8DC7D1",
+            "message": "#șîeu vreau autostrazi in Romania!"
+        },
+        {
+            "id": "3418160981537358_2245141962172605",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQBnLsmvhLXGehvi&w=130&h=130&url=https%3A%2F%2Ffbnewsroomus.files.wordpress.com%2F2018%2F07%2Fsecurity1.png%3Fw%3D960&cfs=1&_nc_cb=1&_nc_hash=AQBx3SH17TxjI1g0",
+            "message": "Hopa! Romania langa UK la fake news. Am ajuns si noi intr-un top, in sfarsit.\n"
+        },
+        {
+            "id": "3418160981537358_2196618553691613",
+            "picture": "https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-0/s130x130/51279109_2196613713692097_1874961388883935232_n.jpg?_nc_cat=108&_nc_sid=8024bb&_nc_ohc=TXfG52TdyN8AX8x2YzS&_nc_ht=scontent.fotp3-3.fna&tp=7&oh=dc143915958d619bfe87c528dd74d681&oe=5F8FB2AB",
+            "message": "Ministerul de Finante nu a reusit tot anul trecut sa atraga tinta de 4 miliarde de lei de la populatie (adica a reusit sub 3 miliarde) si acum dau cu bancile comerciale de pamant, se supara pe ele si apeleaza din nou la populatie cu aceeasi dobanda maxima oferita ca si anul trecut."
+        },
+        {
+            "id": "3418160981537358_2190457497641052",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQApbWPUGSoFcx0Z&w=130&h=130&url=https%3A%2F%2Fstorage.googleapis.com%2Fdeepmind-live-cms%2Fimages%2FSCII-Blog-Thumb-190123-r02.width-600.png&cfs=1&_nc_cb=1&_nc_hash=AQBGLqeLV6COKEOs",
+            "message": "https://deepmind.com/blog/alphastar-mastering-real-time-strategy-game-starcraft-ii/"
+        },
+        {
+            "id": "3418160981537358_2112673542086115",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQDHkSun82URUbHO&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F7L2sUGcOgh0%2Fmaxresdefault.jpg&cfs=1&sx=264&sy=0&sw=720&sh=720&_nc_cb=1&_nc_hash=AQDkseYk0jNcIqtZ",
+            "message": "https://youtu.be/7L2sUGcOgh0"
+        },
+        {
+            "id": "3418160981537358_2010001162353354",
+            "picture": "https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-0/s130x130/42208879_2010001169020020_2145876093280714752_o.jpg?_nc_cat=104&_nc_sid=e007fa&_nc_ohc=jf0Et1vNxA0AX_6T59E&_nc_ht=scontent.fotp3-3.fna&tp=7&oh=c7a41918878385155f47ed6d787a3357&oe=5F90C90D",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_2009993839020753",
+            "picture": "https://scontent.fotp3-1.fna.fbcdn.net/v/t1.0-0/p130x130/42227736_2009993812354089_5365476676073422848_n.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=92rW_Mgxg4kAX8jFVg9&_nc_ht=scontent.fotp3-1.fna&tp=6&oh=a128e9b2daaf3333e78b6a21327aa1d6&oe=5F90F49C",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1990590884294382",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQCW4wAIIxAlXUuO&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FPrqYohBV58o%2Fmaxresdefault.jpg&cfs=1&_nc_cb=1&_nc_hash=AQDH2dYq_TNROfj4",
+            "message": "https://www.youtube.com/watch?v=PrqYohBV58o&feature=youtu.be"
+        },
+        {
+            "id": "3418160981537358_1971036826249788",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQAI2PcmMmJo1hKP&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FSVEU7IawuHg%2Fmaxresdefault.jpg&cfs=1&sx=324&sy=0&sw=720&sh=720&_nc_cb=1&_nc_hash=AQA9DLmSduLpEsr6",
+            "message": "https://www.youtube.com/watch?v=SVEU7IawuHg"
+        },
+        {
+            "id": "3418160981537358_1926501160703355",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQDUnkAcZSh2LUfI&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FR1YcrfLE9JM%2Fhqdefault.jpg&cfs=1&sx=45&sy=0&sw=360&sh=360&_nc_cb=1&_nc_hash=AQA673xlILU3ab4S",
+            "message": "Nicio zi fara gafa...\n"
+        },
+        {
+            "id": "3418160981537358_1924984024188402",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQDYsoZX-oziP7E6&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FcPQZdbFinY4%2Fhqdefault.jpg&cfs=1&sx=31&sy=0&sw=360&sh=360&_nc_cb=1&_nc_hash=AQCuLeKwCzSK_dn7",
+            "message": "https://www.youtube.com/watch?v=cPQZdbFinY4"
+        },
+        {
+            "id": "3418160981537358_1916613958358742",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQBCVW7NOeMvJ8Ic&w=130&h=130&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FaWyhuNEPccI%2Fmaxresdefault.jpg&cfs=1&sx=276&sy=0&sw=720&sh=720&_nc_cb=1&_nc_hash=AQAcLFuETEYEtlYW",
+            "message": "https://www.youtube.com/watch?v=aWyhuNEPccI"
+        },
+        {
+            "id": "3418160981537358_1901019883251483",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQDPneFhyQWNar9W&w=130&h=130&url=http%3A%2F%2Fmedia.hotnews.ro%2Fmedia_server1%2Fimage-2014-01-15-16415618-0-contrib-squareok2.png&cfs=1&_nc_cb=1&_nc_hash=AQBHy_Bb6TDLKzTC",
+            "message": "http://www.contributors.ro/administratie/cum-se-fura-banii-publici-legi-bugete-mijloace-modificarea-codului-penal-%C8%99i-revocarea-conducerii-dna-sunt-doar-ultimele-episoade/"
+        },
+        {
+            "id": "3418160981537358_1894158427270962",
+            "picture": null,
+            "message": "\"Nu aşa se fac codurile penale şi de procedură penală. Actualele coduri au fost redactate şi adoptate în urma unor studii serioase şi de oameni foarte bine pregătiţi în domeniul dreptului penal. De data aceasta, nu îl înţeleg pe domnul ministru al Justiţiei, care este profesor de drept penal, cum a reuşit să lase doi autodidacţi să facă Codul penal şi Codul de procedură penală. Este regretabil că s-a ajuns aici. S-a lucrat atât de mult la actualele coduri şi, în sfârşit, după ani de aplicare, lumea a început să le înţeleagă şi să le aprecieze în spiritul şi litera lor. Acum se fac praf\", a afirmat Augustin Zegrean, fostul preşedinte al Curţii Constituţionale a României."
+        },
+        {
+            "id": "3418160981537358_1889639377722867",
+            "picture": "https://scontent.fotp3-1.fna.fbcdn.net/v/t15.13418-10/s130x130/34292448_202714657046724_3330591282274762752_n.jpg?_nc_cat=107&_nc_sid=ad6a45&_nc_ohc=jfLyOkRHEaIAX-QAF4n&_nc_ht=scontent.fotp3-1.fna&oh=94eefe9edfd53a67423c144c2785cdf0&oe=5F8F2EAC",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1886537528033052",
+            "picture": "https://external.fotp3-3.fna.fbcdn.net/safe_image.php?d=AQDPneFhyQWNar9W&w=130&h=130&url=http%3A%2F%2Fmedia.hotnews.ro%2Fmedia_server1%2Fimage-2014-01-15-16415618-0-contrib-squareok2.png&cfs=1&_nc_cb=1&_nc_hash=AQBHy_Bb6TDLKzTC",
+            "message": "http://www.contributors.ro/cultura/raul-romaniei-de-azi-o-mafie-camuflata-in-partid/"
+        },
+        {
+            "id": "3418160981537358_1588128614540613",
+            "picture": "https://scontent.fotp3-1.fna.fbcdn.net/v/t1.0-0/s130x130/21617568_1588128581207283_5188736682196333838_n.jpg?_nc_cat=110&_nc_sid=7aed08&_nc_ohc=vZ8-FEYFjRcAX9Dz2ho&_nc_ht=scontent.fotp3-1.fna&tp=7&oh=46dc145796d9b552ebde729644f5c217&oe=5F90DAB8",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1544721565547985",
+            "picture": "https://scontent.fotp3-1.fna.fbcdn.net/v/t31.0-0/p130x130/20626760_1544721465547995_3092351096276087037_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=25C5VwXfjLEAX9oSVIx&_nc_ht=scontent.fotp3-1.fna&tp=6&oh=84816d037a021326e32b49cde83ce1c4&oe=5F8EE380",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1512244615462347",
+            "picture": "https://scontent.fotp3-2.fna.fbcdn.net/v/t15.5256-10/p130x130/19675784_792509547597007_4359411871527206912_n.jpg?_nc_cat=106&_nc_sid=ad6a45&_nc_ohc=MXRGBI4xsW8AX-odOp-&_nc_ht=scontent.fotp3-2.fna&oh=b142a8378980413bec3252e673ddbc28&oe=5F8DB11E",
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1490740117612797",
+            "picture": null,
+            "message": null
+        },
+        {
+            "id": "3418160981537358_1480311698655639",
+            "picture": "https://scontent.fotp3-2.fna.fbcdn.net/v/t31.0-0/s130x130/18954751_1480311475322328_333434847994576618_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=QEUCmnLyFAkAX8SaUMz&_nc_ht=scontent.fotp3-2.fna&tp=7&oh=ef74ab38baae90ce6d9a9985656742c7&oe=5F911C12",
+            "message": "In vizita la finuta Iuliana"
+        }
+    ]
+}
+
+export default feedsxH00033;
