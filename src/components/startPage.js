1a101be6155d79ccf05e6d1e14da3263d0f53cad
Loredana <eliaearth@yahoo.com>
2020-09-23 23:40:23 +0300

    fix cors

--- src/components/startPage.js
+++ src/components/startPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import getFeeds from '../utils/api';
-import { cl } from '../utils/utils';
-import feedsxH00033 from '../mocks/feedsxH00033';
+// import feedsxH00033 from '../mocks/feedsxH00033';
 // Import react-bootstrap components
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,47 +7,58 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 // End react-bootstrap components
-const StartPage = (props) => {
-    const path = window.location.pathname;
-    const userId = path.split('/').pop();
-cl(`----------------userID = ${userId} ---------------`)
-    const feeds = feedsxH00033;//getFeeds(userId);
-    console.log('-----------------feeds--------')
-    console.dir(feeds);
-    return (<Container fluid>
-        ------Start page------
-        userId : {userId}
-        {feeds.firstName} {feeds.lastName}<br/><br/>
-        <Row>
-        {feeds.posts.map((feedItem) => {
-            return (
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={feedItem.picture} alt="post" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                {feedItem.message}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
+import Axios from 'axios';
 
-                //--------------- Previous display feed ---------------
-                // <div class="col-md-3" id={feedItem.id}>
-                //     <div class="col-md-6">
-                //         <img src={feedItem.picture} alt="post"/><br/>
-                //     </div>
-                //     <div class="col-md-6">
-                //         <b>{feedItem.message}</b>
-                //     </div>
-                // </div>
+class StartPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUserFeeds: {}
+        }
+    }
 
-            )
-        })}
-        </Row>
-    </Container>)
+    getFeeds = () => {
+        const path = window.location.pathname;
+        const userId = path.split('/').pop(); //xH00012
+        const feedURL = 'https://forum.xhumanity.org/social/feed/';
+        Axios.get(`${feedURL}${userId}`)
+        .then((response) => {
+            this.setState({
+                currentUserFeeds:response.data
+            })
+        })
+        .catch( (err) => {
+        })
+    }
 
+    componentDidMount(){
+        this.getFeeds();
+    }
+    
+    render(){
+        const {currentUserFeeds} = this.state;
+        return (<Container fluid>
+            ------Start page------
+            {currentUserFeeds.firstName} {currentUserFeeds.lastName}<br/><br/>
+            <Row>
+                {currentUserFeeds?.posts?.map((feedItem) => {
+                    return (<div key={feedItem.id}>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={feedItem.picture} alt="post" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        {feedItem.message}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </div>)
+                })}
+            </Row>
+        </Container>)
+    }
 }
 
 export default StartPage;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,43 +1,22 @@
 import Axios from "axios";
-import { cl } from "./utils";
 
-const feedURL1 = 'http://192.168.0.219:8080/social/feed/';
+const feedURL = 'https://forum.xhumanity.org/social/feed/';
 // const feedURL = 'https://192.168.0.219:8443/social/feed/';
-const testURL = 'https://jsonplaceholder.typicode.com/users'
-const config = {headers:{
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json' // 'Content-Type': 'application/json'
-}}
+// const testURL = 'https://jsonplaceholder.typicode.com/users'
+// const config = { xxx: {
+//     'Access-Control-Allow-Origin': '*',
+//     'Content-Type': 'application/json' // 'Content-Type': 'application/json'
+// }}
 
 const getFeeds = (userID) => {
-    const url = `${feedURL1}${userID}`;
-    cl(`------------netwerk call = ${url}`)
-    // Axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-    // Axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-    Axios.get(testURL, config)
-    .then((result) => {
-        console.log('------------response success-----------');
-        console.log(result);
-        return result;
+    Axios.get(`${feedURL}${userID}`)
+    .then((response) => {
+        return response.data;
     })
     .catch( (err) => {
         console.log('------error getting feeds-------')
+        return {}
     })
-    // cl(`----get api call for userID = ${userID}------`)
-    // let request = new XMLHttpRequest();
-    // request.open("GET", url)
-    // request.withCredentials = true;
-    // request.setRequestHeader('Access-Control-Allow-Origin','*')
-    // request.setRequestHeader('Content-Type','JSON')
-    // request.send();
-    // request.onload = () => {
-    //     cl(request);
-    //     if ( request.status === 200 ){
-    //         cl(JSON.parse(request.response))
-    //     }else{
-    //         cl(`errlor ${request.status} ${request.statusText}`)
-    //     }
-    // }
 }
 
 export default getFeeds;
